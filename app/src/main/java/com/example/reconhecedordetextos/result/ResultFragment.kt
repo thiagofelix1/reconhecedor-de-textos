d5fa4dde76574e1c9c978175042ba7ca31813599
thiagofelix1 <thiagofelixdasilva099@gmail.com>
2021-03-03 20:51:29 -0400

    start commit

--- app/src/main/java/com/example/reconhecedordetextos/result/ResultFragment.kt
+++ app/src/main/java/com/example/reconhecedordetextos/result/ResultFragment.kt
@@ -92,7 +92,6 @@ class ResultFragment:Fragment() {
             customAlertDialogView = LayoutInflater.from(contextM)
                     .inflate(R.layout.dialog_language_options , null , false)
 
-
             openDialog()
 
         }
